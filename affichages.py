d1f2de4dbbee6cbf869d9ea70ad2eb5d4286b5a1
Noé-Vermaut <noe.vmt@gmail.com>
2026-05-20 14:20:13 +0200

    Affichages_Corrections

--- affichages.py
+++ affichages.py
@@ -5,7 +5,7 @@ import random
 from PIL import Image
 import Partie_1_Pre_Traitement as pre_traitement
 import Partie_3_Evaluation as eval
-import itertools
+from sklearn.decomposition import PCA
 
 def plot_image_grid(uninfected_dir, parasitized_dir, n_images_per_class=8):
     uninfected_files = [f for f in os.listdir(uninfected_dir) if f.endswith('.png')]
@@ -70,33 +70,3 @@ def plot_losses(losses, title="Evolution de la loss"):
     plt.title(title)
     plt.grid(True, alpha=0.3)
     plt.show()
-
-#Plot de la matrice de confusion
-def plot_confusion_matrix(cm, classes, normalize=False, title='Matrice de Confusion', cmap=plt.cm.Blues):
-    """
-    Cette fonction affiche la matrice de confusion.
-    Normalisation peut être appliquée en définissant `normalize=True`.
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Matrice de confusion normalisée")
-    else:
-        print('Matrice de confusion, sans normalisation')
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.ylabel('Réalité')
-    plt.xlabel('Prédictions du Modèle')
-    plt.tight_layout()
